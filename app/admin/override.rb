16ce9b9afdf709c4cca654dd74018ba0247031c2
Yuichi-K-11 <banaichi1@gmail.com>
2021-01-23 12:43:40 +0000

    [add]gem_activeadmin_override.rb

---
+++ app/admin/override.rb
@@ -0,0 +1,12 @@
+# in app/admin/override.rb
+ActiveAdmin::Devise::SessionsController.class_eval do
+  def after_sign_in_path_for(resource)
+    admin_dashboard_path
+  end
+  def root_path
+    "/"  #add your logic
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+end
