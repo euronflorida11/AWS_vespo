6ce0ad1431c0a04a9d2309436a0708c8e9184156
Yuichi-K-11 <banaichi1@gmail.com>
2021-01-25 12:15:20 +0000

    [update]sport_V_all

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_110216) do
+ActiveRecord::Schema.define(version: 2021_01_25_111917) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_110216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "introduction"
+    t.string "image_id"
   end
 
   create_table "teams", force: :cascade do |t|
