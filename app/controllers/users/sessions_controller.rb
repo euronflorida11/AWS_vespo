d28062445926b6826aa0803e9b87670fbc1dc2cb
Yuichi-K-11 <banaichi1@gmail.com>
2021-01-28 13:42:13 +0000

    [fix]team_enum

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,11 +24,11 @@ class Users::SessionsController < Devise::SessionsController
     @user = User.find_by(email: params[:user][:email].downcase)
     if @user
       if @user.valid_password?(params[:email][:password]) && (@user.active_for_authentication? == false)
-        flash[:error] = "退会済みです。"
+        flash[:error_messages] = "退会済みです。"
         redirect_to about_path
       end
     else
-      flash[:error] = "必須項目を入力してください。"
+      flash[:error_messages] = "必須項目を入力してください。"
     end
   end
 
