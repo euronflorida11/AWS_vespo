e7b3343ccd0d635b05a7687f013bf5a38d78b327
Yuichi-K-11 <banaichi1@gmail.com>
2021-02-06 12:36:51 +0000

    [fix]team_V_map_api

--- lib/vision.rb
+++ lib/vision.rb
@@ -32,7 +32,6 @@ module Vision
       request = Net::HTTP::Post.new(uri.request_uri)
       request['Content-Type'] = 'application/json'
       response = https.request(request, params)
-      
       response_body = JSON.parse(response.body)
       # APIレスポンス出力
       if (error = response_body['responses'][0]['error']).present?
