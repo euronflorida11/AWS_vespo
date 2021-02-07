19d222821d5676ec6834f0d8eb983cbc0058e717
Yuichi-K-11 <banaichi1@gmail.com>
2021-02-07 07:08:37 +0000

    [update]home_V_footer

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,4 +4,21 @@ class HomesController < ApplicationController
 
   def about
   end
+  
+  def index
+    @infos = Info.all
+  end
+
+  def new
+    @info = Info.new
+  end
+
+  def create
+    @info = Info.new(info_params)
+    if @info.save
+      redirect_to about_path, notice: "お知らせを作成しました。"
+    else
+      render :new
+    end
+  end
 end
