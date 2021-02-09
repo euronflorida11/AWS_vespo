5937c7cee5df0607463c6c20f513dd75217405b8
Yuichi-K-11 <banaichi1@gmail.com>
2021-02-09 05:05:39 +0000

    [add]home_V_slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,22 @@
 //= require jquery.jpostal
 //= require_tree .
 
+
+$(function() {
+  $('#slider').slick({
+      infinite: true, //スライドのループ有効化
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      slidesToShow: 1, //表示するスライドの数
+      slidesToScroll: 1, //スクロールで切り替わるスライドの数
+      infinite: true, //スライドのループ有効化
+      centerMode: true, //要素を中央寄せ
+      centerPadding:'10%', //両サイドの見えている部分のサイズ
+      
+  });
+});
+
 $(function() {
   $('#back a').on('click',function(event){
     $('body, html').animate({
