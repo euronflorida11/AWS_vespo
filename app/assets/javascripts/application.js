645e2b2ce11e1cfd911261d532fd91e5e4d5866f
Yuichi-K-11 <banaichi1@gmail.com>
2021-02-13 07:44:08 +0000

    [fix]home_V_turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@
 //= require jquery.jpostal
 //= require_tree .
 
-
+/*global $*/
 $(function() {
   $('#slider').slick({
       infinite: true, //スライドのループ有効化
@@ -31,7 +31,7 @@ $(function() {
       infinite: true, //スライドのループ有効化
       centerMode: true, //要素を中央寄せ
       centerPadding:'10%', //両サイドの見えている部分のサイズ
-      
+
   });
 });
 
