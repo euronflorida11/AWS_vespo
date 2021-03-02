9b17a1140fb43b1ff26aa71f4d2c3fce56158953
Yuichi-K-11 <banaichi1@gmail.com>
2021-03-02 06:46:40 +0000

    [update]user_V_show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def after_sign_in_path_for(resource)
     if user_signed_in?
       flash[:notice] = "ログインしました。"
