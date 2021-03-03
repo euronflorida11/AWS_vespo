0791dcf088e08d1b405a1cbb2a9c56c08bcb9bb2
Yuichi-K-11 <banaichi1@gmail.com>
2021-03-03 14:57:14 +0000

    [update]notification_helper

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,16 +3,16 @@ module NotificationsHelper
   def notification_form(notification)
     @visitor = notification.visitor
     @comment = nil
-    your_team = link_to 'あなたの投稿', team_path(notification), style:"font-weight: bold;"
+    your_team = link_to 'あなたのチーム', team_path(notification), style:"font-weight: bold;"
     @visitor_comment = notification.comment_id
     case notification.action
       when "follow" then
         tag.a(notification.visitor.handle_name, href:user_path(@visitor), style:"font-weight: bold;")+"があなたをフォローしました"
       when "favorite" then
-        tag.a(notification.visitor.handle_name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:team_path(notification.team_id), style:"font-weight: bold;")+"にいいねしました"
+        tag.a(notification.visitor.handle_name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたのチーム', href:team_path(notification.team_id), style:"font-weight: bold;")+"にいいねしました"
       when "comment" then
           @comment = Comment.find_by(id: @visitor_comment)&.comment
-          tag.a(@visitor.handle_name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:team_path(notification.team_id), style:"font-weight: bold;")+"にコメントしました"
+          tag.a(@visitor.handle_name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたのチーム', href:team_path(notification.team_id), style:"font-weight: bold;")+"にコメントしました"
       when "chat" then
         tag.a(notification.visitor.handle_name, href:user_path(@visitor), style:"font-weight: bold;")+"から"+tag.a('あなた', href:chat_path(notification.chat_id), style:"font-weight: bold;")+"へメッセージが届いています"
     end
